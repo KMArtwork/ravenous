13de36541a76366b5f45bbef62ccdd289928ef7f
KMART <93807261+KMArtwork@users.noreply.github.com>
2022-12-28 00:36:37 -0800

    add yelp api fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,29 +3,22 @@ import logo from '../../logo.svg';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import BusinessList from '../BusinessList/BusinessList';
-
-let business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
-
-const businessesArr = [
-  business, business, business, business, business, business
-];
-
+import Yelp from '../../util/Yelp';
 
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {businesses: [],};
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
   searchYelp(term, location, sortBy) {
-    console.log(`Searching for ${term} in ${location}, sorted by ${sortBy}`);
+    Yelp.searchYelp(term, location, sortBy)
+    .then(businesses => {
+      this.setState({businesses: businesses});
+    });
   }
 
   render() {
@@ -33,7 +26,7 @@ class App extends React.Component {
         <div className="App">
           <h1>ravenous</h1>
           <SearchBar searchP={this.searchYelp} />
-          <BusinessList businessP={businessesArr} />
+          <BusinessList business={this.state.businesses} />
         </div>
       );
     }
--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -6,19 +6,19 @@ class Business extends React.Component {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={this.props.businessP.imageSrc} alt=''/>
+                    <img src={this.props.business.imageSrc} alt=''/>
                 </div>
-                <h2>{this.props.businessP.name}</h2>
+                <h2>{this.props.business.name}</h2>
                 <div className="Business-information">
                     <div className="Business-address">
-                        <p>{this.props.businessP.address}</p>
-                        <p>{this.props.businessP.city}</p>
-                        <p>{this.props.businessP.state} {this.props.businessP.zipCode}</p>
+                        <p>{this.props.business.address}</p>
+                        <p>{this.props.business.city}</p>
+                        <p>{this.props.business.state} {this.props.business.zipCode}</p>
                     </div>
                     <div className="Business-reviews">
-                        <h3>{this.props.businessP.category}</h3>
-                        <h3 className="rating">{this.props.businessP.rating}</h3>
-                        <p>{this.props.businessP.reviewCount}</p>
+                        <h3>{this.props.business.category}</h3>
+                        <h3 className="rating">{this.props.business.rating}</h3>
+                        <p>{this.props.business.reviewCount}</p>
                     </div>
                 </div>
             </div>
